34f4a7fd9df5540496770eca8ace37c6b71e11da
Viviane <151794010+vivibiazus@users.noreply.github.com>
2025-11-18 20:46:19 -0300

    Implement MediaSimplesEstrategia class for progress calculation

--- src/model/MediaSimplesEstrategia.py
+++ src/model/MediaSimplesEstrategia.py
@@ -1,1 +1,12 @@
+from .EstrategiaProgresso import EstrategiaProgresso
+
+class MediaSimplesEstrategia(EstrategiaProgresso):
+    def calcular(self, trilha):
+        cursos = trilha.cursos
+        if not cursos:
+            return 0.0
+        soma = 0.0
+        for c in cursos:
+            soma += c.progresso()
+        return soma / float(len(cursos))
 
