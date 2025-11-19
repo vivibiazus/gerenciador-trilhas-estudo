2eefcafab649a67b0a2ca86d9426eef611eb1434
Viviane <151794010+vivibiazus@users.noreply.github.com>
2025-11-19 09:25:44 -0300

    Update teste_factory.py

--- src/tests/teste_factory.py
+++ src/tests/teste_factory.py
@@ -5,24 +5,27 @@ from model.TarefaQuiz import TarefaQuiz
 from model.TarefaPratica import TarefaPratica
 from model.TarefaProjeto import TarefaProjeto
 
+def eh_classe(obj, nome_classe: str) -> bool:
+    return obj.__class__.__name__ == nome_classe
+
 print("\n=== Teste da TarefaFactory ===")
 
 # 1) Criação por STRING
 q = TarefaFactory.criar("quiz", titulo="Prova 1", nota=8, nota_max=10)
-print(isinstance(q, TarefaQuiz), q.exibir_dados(), sep="\n")
+print(eh_classe(q, "TarefaQuiz"), q.exibir_dados(), sep="\n")
 
 l = TarefaFactory.criar("leitura", titulo="Capítulo 1", total_paginas=50, paginas_lidas=20)
-print(isinstance(l, TarefaLeitura), l.exibir_dados(), sep="\n")
+print(eh_classe(l, "TarefaLeitura"), l.exibir_dados(), sep="\n")
 
 p = TarefaFactory.criar("pratica", titulo="Lab 1", total_etapas=6, etapas_concluidas=3)
-print(isinstance(p, TarefaPratica), p.exibir_dados(), sep="\n")
+print(eh_classe(p, "TarefaPratica"), p.exibir_dados(), sep="\n")
 
 pj = TarefaFactory.criar("projeto", titulo="Projeto Final", total_entregas=4, entregas_aprovadas=1)
-print(isinstance(pj, TarefaProjeto), pj.exibir_dados(), sep="\n")
+print(eh_classe(pj, "TarefaProjeto"), pj.exibir_dados(), sep="\n")
 
 # 2) Criação por ENUM
 l2 = TarefaFactory.criar(TipoTarefaEstudo.LEITURA, titulo="Capítulo 2", total_paginas=30, paginas_lidas=15)
-print(isinstance(l2, TarefaLeitura), l2.exibir_dados(), sep="\n")
+print(eh_classe(l2, "TarefaLeitura"), l2.exibir_dados(), sep="\n")
 
 # 3) Erros esperados (campos obrigatórios faltando)
 try:
